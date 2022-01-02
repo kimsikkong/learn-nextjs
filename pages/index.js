33b6cf184bfffd5919ab3a66c646bbbf5456cf9d
kimsikkong <kimsikkong@gmail.com>
2022-01-02 17:42:10 +0900

    implement dynamic routes (posts)

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Layout, {siteTitle} from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import {getStoredPostsData} from "../lib/post";
+import {getStoredPostsData} from "../lib/posts";
 
 export default function Home({allPostsData}) {
     return (
